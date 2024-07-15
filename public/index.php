04cb1db7131de2bef9565159d1c605a8bec5ecb0
Elvijs <zepselvijs18@gmail.com>
2024-07-16 00:07:21 +0300

    master created file to display all products,delete products, or go to another page to add products

---
+++ public/index.php
@@ -0,0 +1,59 @@
+<?php
+// Showing data from the database and handling delete functionality
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+require_once __DIR__ . "/../conn/connection.php";
+require_once __DIR__ . "/../services/ProductService.php";
+
+$connection = new Connection();
+$conn = $connection->getConnection();
+
+if (!$conn) {
+    die("Failed to connect to the database.");
+}
+
+$productService = new ProductService($conn);
+
+// Fetch data from the products table
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['action']) && $_GET['action'] == 'fetch_products') {
+    $products = $productService->getAllProducts();
+    echo json_encode($products);
+    exit();
+}
+
+// Handle delete functionality
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['delete_ids'])) {
+    $delete_ids = $_POST['delete_ids'];
+    $productService->deleteProducts($delete_ids);
+    header("Location: index.php");
+    exit();
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Product List Page</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style/listStyle.css"/>
+</head>
+<body>
+    <div class="headerContainer">
+        <h1>Product List</h1>
+        <div class="button-group">
+            <a href="addProductPage.php" class="link">
+                <button type="button" class="btn">ADD</button>
+            </a>
+            <button type="submit" form="product-form" id="delete-product-btn" class="btn">MASS DELETE</button>
+        </div>
+    </div>
+    <form id="product-form" method="POST">
+        <div class="container" id="product-container">
+            <!-- Products will be dynamically inserted here -->
+        </div>
+    </form>
+    <footer class="endName">Scandiweb Test assignment</footer>
+    <script src="/backend/script.js"></script>
+</body>
+</html>
